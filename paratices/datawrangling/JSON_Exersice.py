834b6633e066b74a088e26e48a95a5db68362da6
enixdark <cqshinn92@gmail.com>
2014-11-25 14:29:00 +0700

    add

--- paratices/datawrangling/JSON_Exersice.py
+++ paratices/datawrangling/JSON_Exersice.py
@@ -1,1 +1,12 @@
-__author__ = 'enixdark'
+import json
+import requests
+import pprint
+import ast
+
+def api_get_request(url):
+    # In this exercise, you want to call the last.fm API to get a list of the
+    # top artists in Spain.
+    #
+    # Once you've done this, return the name of the number 1 top artist in Spain.
+    data = ast.literal_eval(json.loads(requests.get(url).text.strip()))
+    return data['topartists']['artist'][0]['name'] # return the top artist in Spain
--- paratices/datawrangling/SQL_Exersice.py
+++ paratices/datawrangling/SQL_Exersice.py
@@ -19,4 +19,167 @@ def select_first_50(filename):
 
     #Execute your SQL command against the pandas frame
     aadhaar_solution = pandasql.sqldf(q.lower(), locals())
-    return aadhaar_solution    
+    return aadhaar_solution
+
+def aggregate_query(filename):
+    # Read in our aadhaar_data csv to a pandas dataframe.  Afterwards, we rename the columns
+    # by replacing spaces with underscores and setting all characters to lowercase, so the
+    # column names more closely resemble columns names one might find in a table.
+
+    aadhaar_data = pandas.read_csv(filename)
+    aadhaar_data.rename(columns = lambda x: x.replace(' ', '_').lower(), inplace=True)
+
+    # Write a query that will select from the aadhaar_data table how many men and how
+    # many women over the age of 50 have had aadhaar generated for them in each district
+    #
+    # Note that in this quiz, the SQL query keywords are case sensitive.
+    # For example, if you want to do a sum make sure you type 'sum' rather than 'SUM'.
+    #
+
+    # The possible columns to select from aadhaar data are:
+    #     1) registrar
+    #     2) enrolment_agency
+    #     3) state
+    #     4) district
+    #     5) sub_district
+    #     6) pin_code
+    #     7) gender
+    #     8) age
+    #     9) aadhaar_generated
+    #     10) enrolment_rejected
+    #     11) residents_providing_email,
+    #     12) residents_providing_mobile_number
+    #
+    # You can download a copy of the aadhaar data that we are passing
+    # into this exercise below:
+    # https://www.dropbox.com/s/vn8t4uulbsfmalo/aadhaar_data.csv
+
+    q = "select gender,district,sum(aadhaar_generated) from aadhaar_data where age > 50 group by gender,district"
+
+    # Execute your SQL command against the pandas frame
+    aadhaar_solution = pandasql.sqldf(q.lower(), locals())
+    return aadhaar_solution
+
+
+def num_rainy_days(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return one column and
+    one row - a count of the number of days in the dataframe where
+    the rain column is equal to 1 (i.e., the number of days it
+    rained).  The dataframe will be titled 'weather_data'. You'll
+    need to provide the SQL query.  You might find SQL's count function
+    useful for this exercise.  You can read more about it here:
+
+    https://dev.mysql.com/doc/refman/5.1/en/counting-rows.html
+
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be equal to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply
+    where maxtempi = 76.
+
+    You can see the weather data that we are passing in below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = 'select count(*) from weather_data where rain = 1'
+
+
+    #Execute your SQL command against the pandas frame
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days
+
+def max_temp_aggregate_by_fog(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return two columns and
+    two rows - whether it was foggy or not (0 or 1) and the max
+    maxtempi for that fog value (i.e., the maximum max temperature
+    for both foggy and non-foggy days).  The dataframe will be
+    titled 'weather_data'. You'll need to provide the SQL query.
+
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be useful to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply
+    where maxtempi = 76.
+
+    You can see the weather data that we are passing in below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = 'select fog,max(cast (maxtempi as integer)) from weather_data group by fog'
+
+
+    #Execute your SQL command against the pandas frame
+    rainy_days = pandasql.sqldf(q.lower(), locals())
+    return rainy_days
+
+def avg_min_temperature(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data.  The SQL query should return one column and
+    one row - the average meantempi on days that are a Saturday
+    or Sunday (i.e., the the average mean temperature on weekends).
+    The dataframe will be titled 'weather_data' and you can access
+    the date in the dataframe via the 'date' column.
+
+    You'll need to provide  the SQL query.
+
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be equal to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply
+    where maxtempi = 76.
+
+    Also, you can convert dates to days of the week via the 'strftime' keyword in SQL.
+    For example, cast (strftime('%w', date) as integer) will return 0 if the date
+    is a Sunday or 6 if the date is a Saturday.
+
+    You can see the weather data that we are passing in below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = """
+    select  avg(cast (meantempi as integer))
+            from weather_data where cast(strftime("%w", date) as integer) in (0,6)
+            """
+
+    #Execute your SQL command against the pandas frame
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends
+
+def avg_min_temperature(filename):
+    '''
+    This function should run a SQL query on a dataframe of
+    weather data. More specifically you want to find the average
+    minimum temperature on rainy days where the minimum temperature
+    is greater than 55 degrees.
+
+    You might also find that interpreting numbers as integers or floats may not
+    work initially.  In order to get around this issue, it may be equal to cast
+    these numbers as integers.  This can be done by writing cast(column as integer).
+    So for example, if we wanted to cast the maxtempi column as an integer, we would actually
+    write something like where cast(maxtempi as integer) = 76, as opposed to simply
+    where maxtempi = 76.
+
+    You can see the weather data that we are passing in below:
+    https://www.dropbox.com/s/7sf0yqc9ykpq3w8/weather_underground.csv
+    '''
+    weather_data = pandas.read_csv(filename)
+
+    q = """
+    select avg(cast (mintempi as integer)) from weather_data where cast(mindewpti as integer) >=55
+    and rain = 1
+    """
+
+    #Execute your SQL command against the pandas frame
+    mean_temp_weekends = pandasql.sqldf(q.lower(), locals())
+    return mean_temp_weekends
