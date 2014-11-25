7603ab7f44f550873e608b829e57a35c044f0c35
enixdark <cqshinn92@gmail.com>
2014-11-25 14:29:40 +0700

    add

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-__author__ = 'enixdark'
+import os
+
+BASE_DIR = os.path.dirname(__file__)
+DATA_DIR = os.path.join(os.path.dirname(__file__),'data')
+URL_API = 'http://www.last.fm/api/show/geo.getTopArtists'
